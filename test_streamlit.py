545c473ada9213a9018651635e68d626cc07d3eb
ClegL <161335201+ClegL@users.noreply.github.com>
2024-05-03 11:56:03 +0200

    Add files via upload

--- test_streamlit.py
+++ test_streamlit.py
@@ -1,22 +1,50 @@
 import streamlit as st
 import pandas as pd
-
-st.title('Hello Wilders, welcome to my application!')
-
-st.write("I enjoy to discover streamlit possibilities !")
-
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-df_weather = pd.read_csv(link)
-df_weather
-
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
-
-colonnes_num = df_weather.select_dtypes(include=['number'])
-
 import seaborn as sns
-viz_correlation = sns.heatmap(colonnes_num.corr(), 
-								center=0,
-								cmap = sns.color_palette("vlag", as_cmap=True)
-								)
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.pyplot(viz_correlation.figure)
+df_cars = pd.read_csv('https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv')
+
+cols_num = df_cars.select_dtypes(include=['number']).columns
+
+st.subheader('Analyse de corrélation')
+corr = df_cars[cols_num].corr()
+fig, ax = plt.subplots(figsize=(10, 8))
+sns.heatmap(corr, annot=True, cmap='coolwarm', fmt=".2f", ax=ax)
+st.pyplot(fig)
+
+# Nous pouvons constater des corrélations évidentes entre les colonnes hp
+# cubicinches, cylinders and weights puisqu'elles concernent la 
+# partie mécanique contrairement à year et time-to-60 qui ont très peu voir pas
+# de corrélations avec les colonnes citées précédemment.
+
+pairplot = sns.pairplot(df_cars[cols_num])
+st.pyplot(pairplot.fig)
+
+#Les pairplots permettent de confirmer les corrélations vues précédemment
+
+cols_num = ['mpg', 'cylinders', 'cubicinches', 'hp', 'weightlbs', 'time-to-60']
+df_cars[cols_num] = df_cars[cols_num].apply(pd.to_numeric, errors='coerce')
+df_cars.dropna(subset=cols_num, inplace=True)
+
+for col in cols_num:
+    st.subheader(f'Distribution de {col}')
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.histplot(df_cars[col], kde=True, ax=ax)
+    plt.xlabel(col)
+    plt.ylabel('Fréquence')
+    st.pyplot(fig)
+
+# Nous voyons ici que les courbes sont globalement descendantes
+# montrant des distributions inégales sauf pour la colonne
+# 0-to-60
+
+unique_values = df_cars['continent'].unique()
+
+for value in unique_values:
+    if st.button(value):
+      
+        filtered_df = df_cars[df_cars['continent'] == value]
+        
+        st.write(filtered_df)
